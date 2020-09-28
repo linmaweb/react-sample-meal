5e050c681ba1cc89afbe9bf0cd6abc6d4c6e6abc
Lin <linmaweb@gmail.com>
2020-09-28 17:33:09 +1000

    components updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MealsState } from "../../config";
+import Title from "../Title/Title";
 import Home from "../Home/Home";
 import SingleMeal from "../SingleMeal/SingleMeal";
 import "./App.css";
 
 const App = () => {
   return (
-    <MealsState>
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/meal/:id" component={SingleMeal} />
-      </Router>
-    </MealsState>
+    <>
+      <Title type="Meal" />
+      <MealsState>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/meal/:id" component={SingleMeal} />
+        </Router>
+      </MealsState>
+    </>
   );
 };
 
