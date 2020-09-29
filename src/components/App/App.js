99489e1590a5b832e5849904b34e968f34ecd19e
Lin <linmaweb@gmail.com>
2020-09-30 01:50:43 +1000

    components updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,128 +8,118 @@ import SingleMeal from "../SingleMeal/SingleMeal";
 import "./App.css";
 
 const App = () => {
-  const MealsState = ({ children }) => {
-    const [search, setSearch] = useState("");
-    const [recipes, setRecipes] = useState([]);
-    const [first, setFirst] = useState(SEARCH_DEFAULT.from);
-    const [last, setLast] = useState(SEARCH_DEFAULT.to);
-    const [loading, setLoading] = useState(false);
-    const [isHidden, setIsHidden] = useState(false);
-    const [error, setError] = useState(false);
+  const [search, setSearch] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [first, setFirst] = useState(SEARCH_DEFAULT.from);
+  const [last, setLast] = useState(SEARCH_DEFAULT.to);
+  const [loading, setLoading] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+  const [error, setError] = useState(false);
 
-    const URL_DEFAULT = `${URL_CORS}${URL_BASE}${SEARCH_DEFAULT.default}&${URL_API}&from=${SEARCH_DEFAULT.from}&to=${SEARCH_DEFAULT.to}`;
-    const URL_SEARCH = `${URL_CORS}${URL_BASE}${search}&${URL_API}&from=${first}&to=${last}`;
+  const URL_DEFAULT = `${URL_CORS}${URL_BASE}${SEARCH_DEFAULT.default}&${URL_API}&from=${SEARCH_DEFAULT.from}&to=${SEARCH_DEFAULT.to}`;
+  const URL_SEARCH = `${URL_CORS}${URL_BASE}${search}&${URL_API}&from=${first}&to=${last}`;
 
-    const searchDefault = () => {
-      setLoading(true);
-      fetch(URL_DEFAULT)
-        .then((res) => {
-          if (!res.ok) {
-            setError(true);
-            throw Error(res.statusText);
-          }
-          return res;
-        })
-        .then((res) => res.json())
-        .then((data) => setRecipes(data.hits))
-        .catch((error) => {
-          console.log(error.message);
-          setLoading(false);
+  const searchDefault = () => {
+    setLoading(true);
+    fetch(URL_DEFAULT)
+      .then((res) => {
+        if (!res.ok) {
           setError(true);
-          return;
-        });
-      window.scrollTo(0, 0);
-      setLoading(false);
-    };
-
-    const searchRecipes = () => {
-      setLoading(true);
-      setError(false);
-      fetch(URL_SEARCH)
-        .then((res) => {
-          if (!res.ok) {
-            setError(true);
-            throw Error(res.statusText);
-          }
-          return res;
-        })
-        .then((res) => res.json())
-        .then((data) => {
-          if (search.trim() !== "" && !data.more) {
-            setLoading(false);
-            setError(true);
-            return;
-          }
-          return setRecipes(data.hits);
-        })
-        .catch((error) => {
-          console.log(error.message);
-          setLoading(false);
-          setError(true);
-          return;
-        });
-      window.scrollTo(0, 0);
-      setLoading(false);
-    };
-
-    const getRecipes = (e) => {
-      e.preventDefault();
-      if (search.trim() === "") {
+          throw Error(res.statusText);
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((data) => setRecipes(data.hits))
+      .catch((error) => {
+        console.log(error.message);
+        setLoading(false);
+        setError(true);
         return;
-      }
-      setIsHidden(true);
-      searchRecipes();
-    };
-
-    const handlePagination = (direction) => {
-      if (direction === "next") {
-        setFirst((prevRecipe) => prevRecipe + SEARCH_DEFAULT.to);
-        setLast((prevRecipe) => prevRecipe + SEARCH_DEFAULT.to);
-        return;
-      }
-      if (direction === "previous" && first !== 0) {
-        setFirst((prevRecipe) => prevRecipe - SEARCH_DEFAULT.to);
-        setLast((prevRecipe) => prevRecipe - SEARCH_DEFAULT.to);
-      }
-    };
-
-    useEffect(() => {
-      searchDefault();
-    }, []);
-
-    useEffect(() => {
-      searchRecipes();
-    }, [first, last]);
-
-    return (
-      <MealsContext.Provider
-        value={{
-          recipes,
-          setRecipes,
-          getRecipes,
-          search,
-          setSearch,
-          isHidden,
-          error,
-          loading,
-          handlePagination,
-        }}
-      >
-        {children}
-      </MealsContext.Provider>
-    );
+      });
+    window.scrollTo(0, 0);
+    setLoading(false);
   };
 
+  const searchRecipes = () => {
+    setLoading(true);
+    setError(false);
+    fetch(URL_SEARCH)
+      .then((res) => {
+        if (!res.ok) {
+          setError(true);
+          throw Error(res.statusText);
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (search.trim() !== "" && !data.more) {
+          setLoading(false);
+          setError(true);
+          return;
+        }
+        return setRecipes(data.hits);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setLoading(false);
+        setError(true);
+        return;
+      });
+    window.scrollTo(0, 0);
+    setLoading(false);
+  };
+
+  const getRecipes = (e) => {
+    e.preventDefault();
+    if (search.trim() === "") {
+      return;
+    }
+    setIsHidden(true);
+    searchRecipes();
+  };
+
+  const handlePagination = (direction) => {
+    if (direction === "next") {
+      setFirst((prevRecipe) => prevRecipe + SEARCH_DEFAULT.to);
+      setLast((prevRecipe) => prevRecipe + SEARCH_DEFAULT.to);
+      return;
+    }
+    if (direction === "previous" && first !== 0) {
+      setFirst((prevRecipe) => prevRecipe - SEARCH_DEFAULT.to);
+      setLast((prevRecipe) => prevRecipe - SEARCH_DEFAULT.to);
+    }
+  };
+
+  useEffect(() => {
+    searchDefault();
+  }, []);
+
+  useEffect(() => {
+    searchRecipes();
+  }, [first, last]);
+
   return (
-    <>
+    <MealsContext.Provider
+      value={{
+        recipes,
+        setRecipes,
+        getRecipes,
+        search,
+        setSearch,
+        isHidden,
+        error,
+        loading,
+        handlePagination,
+      }}
+    >
       <Title type="Meal" />
-      <MealsState>
-        <Router>
-          <Route exact path="/" component={Home} />
-          <Route path="/meal/:id" component={SingleMeal} />
-        </Router>
-      </MealsState>
-    </>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/meal/:id" component={SingleMeal} />
+      </Router>
+    </MealsContext.Provider>
   );
 };
 
